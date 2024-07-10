2ba5d1cbe5177d6853631e5ff99f34d1bf099ae1
Azmal <azmalgazi8@gmail.com>
2024-07-10 11:51:56 +0600

    Project basic setup done

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-4xl">Wilder nest</h1>
+      <Button>Hello</Button>
+    </div>
+  );
+};
+
+export default App;
